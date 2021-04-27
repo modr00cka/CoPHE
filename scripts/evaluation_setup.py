eb1ccdea5a6b6d2e4f7dc1a4b5389b25c38bc295
Matus Falis <s1206296@cyclades.inf.ed.ac.uk>
2021-04-27 21:17:13 +0100

    Initial commit for layer-based ICD9 evaluation.

---
+++ scripts/evaluation_setup.py
@@ -0,0 +1,78 @@
+import numpy as np
+from multi_level_eval import low_level_diagonal
+
+
+def setup_matrices_by_layer(code_ids, translation_dict, max_layer = 1, include_duplicates = True):
+    """
+    Sets up the transition matrices and ID dictionaries for each layer of the ontology up to a maximum value (from the bottom up).
+    sample_code_ids - a dictionary mapping IDs in the output layer to codes
+    translation_dict - a dictionary containing the codes' ordered parent list (coming from the .json file provided in the ICD9 folder)
+    max_layer - integer maximum layer of the ontology (from the bottom up) up to which the hierarchical evaluation is applied
+    include_duplicates - boolean, default = True; maintains duplication across lower layers if a leaf is not present in the lowest layer (results in presence of all leafs in all layers)
+    returns a tuple:
+        matrices - a list of transition matrices from the leaves to each layer of the ontology up to max_layer (from bottom up)
+        layer_id_dicts - a list of dictionaries of code IDs in vectors for each layer of the ontology up to max_layer (from the bottom up)
+    """
+    matrices = [] # tranlsation matrices per layer
+    layer_id_dicts = [] # id-to-code dictionary per layer
+    for layer in range(max_layer):
+        rows, cols, vals = [], [], [] # setup for a sparse matrix
+        layer_codeset = set() # codeset for ancestors - in order to remove duplicates for layer representation
+        for code in code_ids:
+            layer_codeset.add(translation_dict[code]["parents"][layer]) # collection of relevant ancestors in the layer
+            
+        layer_ranges = list(range(len(layer_codeset)))
+        layer_id_dict = dict(zip(list(layer_codeset), layer_ranges)) # association of IDs with relevant acestors in the layer
+        
+        for code in code_ids:
+            rows.append(code_ids[code])  # row number (current code)
+            ancestor = translation_dict[code]["parents"][layer]
+            cols.append(layer_id_dict[ancestor]) # col number (ancestor)
+            if include_duplicates or layer == max_layer-2:  # if duplicates are allowed or the next layer is the final layer, create an edge
+                vals.append(1)
+            else:  # otherwise observe the ancestor of the ancestor - if this matches the current code, do not create an edge. Otherwise create an edge.
+                double_ancestor = translation_dict[ancestor]["parents"][layer]  
+                duplicate_ancestor = double_ancestor == code
+                vals.append(not(duplicate_ancestor)*1)
+                    
+        
+        matrix = csr_matrix((vals, (rows, cols)), shape=((len(rows)), (len(set(cols))))) # set up the sparse matrix
+        
+        matrices.append(matrix) # append the matrix for this layer
+        layer_id_dicts.append(layer_id_dict) # append the id dictionary for this layer
+    return matrices, layer_id_dicts 
+
+def low_level_diagonal(matrix):
+    """
+    Creates a diagonal matrix from the input. Intended as a filter for the lowest level of the ontology
+    """
+    bin_vector = np.sum(matrix, axis = 1)
+    return np.diag(bin_vector)
+                
+def hierarchical_eval_setup(preds, golds, layer_matrices, max_onto_layers):
+    """
+    inputs:
+      preds - a numpy array, a matrix of predictions
+      golds - a numpy array, a matrix of true labels
+      layer_matrices - a list of numpy arrays translating the leaf nodes into layers of the ontology
+      max_onto_layers - an integer describing the maximum layer (from the bottom up) within the ontology to be evaluated on
+    """
+    
+    # isolation of leaves appearing at the lowest level
+    lowest_layer_filter = low_level_diagonal(layer_matrices[0]) 
+    combined_preds = [preds*lowest_layer_filter]
+    combined_golds = [golds*lowest_layer_filter]
+    
+    # handling further layers
+    for i in range(max_onto_layers):
+        translation_matrix = layer_matrices[i] # layer matrix retrieval
+        translated_preds, translated_golds = preds*translation_matrix, golds*translation_matrix # translation from flat predictions into the layer
+        combined_preds.append(translated_preds)
+        combined_golds.append(translated_golds)
+    
+    # concatenation between layers for predictions and true labels respectively
+    combined_preds = np.concatenate(combined_preds, 1)
+    combined_golds = np.concatenate(combined_golds, 1)
+    
+    return combined_preds, combined_golds
+    
---
+++ scripts/multi_level_eval.py
@@ -0,0 +1,217 @@
+import pandas as pd
+from scipy.sparse import csr_matrix
+import numpy as np
+
+
+import logging
+
+def tp_matrix_mul(pred, gold, axes):
+    """
+    Calculation of True Positives in non-binary setting.
+    On the ancestor levels leaf-level mismatches do not matter. If an ancestor-prediction has an ancestor-gold counterpart, it is considered a TP. Hence, the overall TP for an ancestor is the minimum of the count of the predicted ancestor and the count of the gold standard ancestor.
+    inputs
+      pred: numpy array of predictions
+      gold: numpy array of true labels
+      axes: axes on which summing is to be performed (all dimensions for overall TP)
+    returns integer if axes represent all dimensions, a vector of integers otherwise 
+    """
+    return np.sum(np.minimum(pred, gold), axis=axes)
+
+def fp_matrix_mul(pred, gold, axes):
+    """
+    Calculation of False Positives in non-binary setting.
+    If an ancestor-prediction does not have an ancestor-gold counterpart, it is considered a FP. Hence, the overall FP for an ancestor represents how many more times the ancestor has been predicted in a document compared to how many times it appears in the gold standard.
+    inputs
+      pred: numpy array of predictions
+      gold: numpy array of true labels
+      axes: axes on which summing is to be performed (all dimensions for overall FP)
+    returns integer if axes represent all dimensions, a vector of integers otherwise 
+    """
+    return np.sum(np.maximum(pred - gold, 0), axis=axes)
+
+def fn_matrix_mul(pred, gold, axes):
+    """
+    Calculation of False Negatives in non-binary setting.
+    If an ancestor-gold does not have an ancestor-prediction counterpart, it is considered a FN. Hence, the overall FN for an ancestor represents how many more times the ancestor appears in a document compared to how many times it was predicted for the document.
+    inputs
+      pred: numpy array of predictions
+      gold: numpy array of true labels
+      axes: axes on which summing is to be performed (all dimensions for overall FN)
+    returns integer if axes represent all dimensions, a vector of integers otherwise 
+    """
+    return np.sum(np.maximum(gold - pred, 0), axis=axes)
+    
+    
+def tp_matrix_mul_full(pred, gold, axes = (0,1)):
+    """
+    Overall TP for a non-binary 2d matrix
+    returns integer
+    """
+    return tp_matrix_mul(pred, gold, axes)
+    
+def fp_matrix_mul_full(pred, gold, axes = (0,1)):
+    """
+    Overall FP for a non-binary 2d matrix
+    returns integer
+    """
+    return fp_matrix_mul(pred, gold, axes)
+    
+def fn_matrix_mul_full(pred, gold, axes = (0,1)):
+    """
+    Overall FN for a non-binary 2d matrix
+    returns integer
+    """
+    return fn_matrix_mul(pred, gold, axes)
+    
+    
+def tp_matrix_mul_per_class(pred, gold, axes = 0):
+    """
+    per-class TP for a non-binary 2d matrix
+    returns 1d np.array
+    """
+    return tp_matrix_mul(pred, gold, axes)
+    
+def fp_matrix_mul_per_class(pred, gold, axes = 0):
+    """
+    per-class FP for a non-binary 2d matrix
+    returns 1d np.array
+    """
+    return fp_matrix_mul(pred, gold, axes)
+    
+def fn_matrix_mul_per_class(pred, gold, axes = 0):
+    """
+    per-class FN for a non-binary 2d matrix
+    returns 1d np.array
+    """
+    return fn_matrix_mul(pred, gold, axes)
+
+def report(pred, gold, code_id_dict):
+    """
+    Creates a per-class dataframe report.
+    This includes the Precision, Recall, F1 score, Support in the evaluation set, and the code itself.
+    inputs:
+        pred          2d np.array prediction matrix
+        gold          2d np.array matrix of gold standard labels
+        code_id_dict  dictionary mapping codes to their ID in the prediction/gold vectors
+    returns Pandas DataFrame
+    """
+    
+    # Calculation of TP/FP/FN per class
+    tp = tp_matrix_mul_per_class(pred, gold)
+    fp = fp_matrix_mul_per_class(pred, gold)
+    fn = fn_matrix_mul_per_class(pred, gold)
+    
+    # Calculation of the support within the evaluation set
+    support = np.sum(gold, axis = 0)
+    
+    
+    # Precision
+    prec_denom = tp+fp
+    prec_denom_corrected = prec_denom + (prec_denom == 0)*1
+    prec = tp/prec_denom_corrected
+    
+    # Recall
+    rec_denom = tp+fn
+    rec_denom_corrected = rec_denom + (rec_denom == 0)*1
+    rec = tp/(rec_denom_corrected)
+    
+    # F1 score
+    f1_denom = prec+rec
+    f1_denom_corrected = f1_denom+((f1_denom == 0)*1)
+    f1 = 2*(prec*rec)/(f1_denom_corrected)
+    
+    # matchin codes
+    code_ids = sorted(code_id_dict)
+    codes = [code_id_dict[k] for k in code_ids]
+    
+    df = pd.DataFrame(list(zip(prec, rec, f1, support, codes)),
+                                              columns =['Precision', "Recall", "F1", "Support", "Code"])
+    return df
+
+def report_micro(pred, gold):
+    """
+    Creates an overall report on the micro lvel.
+    This includes the micro Precision, Recall, F1 score.
+    inputs:
+        pred          2d np.array prediction matrix
+        gold          2d np.array matrix of gold standard labels
+        code_id_dict  dictionary mapping codes to their ID in the prediction/gold vectors
+    returns a dictionary with real values for "Precision", "Recall", and "F1"
+    """
+    tp = tp_matrix_mul_full(pred, gold)
+    fp = fp_matrix_mul_full(pred, gold)
+    fn = fn_matrix_mul_full(pred, gold)
+    
+    prec_denom = tp+fp
+    prec_denom_corrected = prec_denom + (prec_denom == 0)*1
+    prec_micro = tp/prec_denom_corrected
+    
+    rec_denom = tp+fn
+    rec_denom_corrected = rec_denom + (rec_denom == 0)*1
+    rec_micro = tp/(rec_denom_corrected)
+    
+    f1_denom = prec_micro+rec_micro
+    f1_denom_corrected = f1_denom+((f1_denom == 0)*1)
+    f1 = 2*(prec_micro*rec_micro)/(f1_denom_corrected)
+    
+    report_dict = dict({"Precision":prec_micro, "Recall":rec_micro,"F1":f1})
+    
+    return report_dict
+    
+def report_macro(pred, gold):
+    """
+    Creates an overall report on the macro lvel.
+    This includes the macro Precision, Recall, F1 score.
+    inputs:
+        pred          2d np.array prediction matrix
+        gold          2d np.array matrix of gold standard labels
+        code_id_dict  dictionary mapping codes to their ID in the prediction/gold vectors
+    returns a dictionary with real values for "Precision", "Recall", and "F1"
+    """
+    tp = tp_matrix_mul_per_class(pred, gold)
+    fp = fp_matrix_mul_per_class(pred, gold)
+    fn = fn_matrix_mul_per_class(pred, gold)
+    
+    prec_denom = tp+fp
+    prec_denom_corrected = prec_denom + (prec_denom == 0)*1
+    prec = tp/prec_denom_corrected
+    prec_macro = np.average(prec, axis = 0)
+    
+    rec_denom = tp+fn
+    rec_denom_corrected = rec_denom + (rec_denom == 0)*1
+    rec = tp/(rec_denom_corrected)
+    rec_macro = np.average(rec, axis = 0)
+    
+    f1_denom = prec_macro+rec_macro
+    f1_denom_corrected = f1_denom+((f1_denom == 0)*1)
+    f1 = 2*(prec_macro*rec_macro)/(f1_denom_corrected)
+    report_dict = dict({"Precision":prec_macro, "Recall":rec_macro,"F1":f1})
+    
+    return report_dict
+
+def report_macro_bin(pred, gold):
+    """
+    binarised version of report_macro - the prediction and gold matrix are set to binary, where positive entries are set to 1.
+    return report_macro on these binarised inputs
+    """
+    pred_bin = (pred>0)*1
+    gold_bin = (gold>0)*1
+    return report_macro(pred_bin, gold_bin)
+
+def report_micro_bin(pred, gold):
+    """
+    binarised version of report_micro - the prediction and gold matrix are set to binary, where positive entries are set to 1.
+    return report_micro on these binarised inputs
+    """
+    pred_bin = (pred>0)*1
+    gold_bin = (gold>0)*1
+    return report_micro(pred_bin, gold_bin)
+
+def report_bin(pred, gold):
+    """
+    Creates a per-class dataframe report on binarised inputs.
+    """
+    pred_bin = (pred>0)*1
+    gold_bin = (gold>0)*1
+    return report(pred_bin, gold_bin)
+    
